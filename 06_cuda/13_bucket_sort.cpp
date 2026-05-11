54e10d6e0e0634f34ac2c29ccc12116e6bd928dc
Tohma Nozawa <nozawa.t.757f@m.isct.ac.jp>
2026-05-11 18:46:41 +0900

    Submit Lecture 6 homework

--- 06_cuda/13_bucket_sort.cpp
+++ 06_cuda/13_bucket_sort.cpp
@@ -1,32 +1,50 @@
 #include <cstdio>
 #include <cstdlib>
-#include <vector>
+
+__global__ void init_bucket(int *bucket, int range) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < range) bucket[i] = 0;
+}
+
+__global__ void add_bucket(int *key, int *bucket, int n) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n) atomicAdd(&bucket[key[i]], 1);
+}
 
 int main() {
-  int n = 50;
-  int range = 5;
-  std::vector<int> key(n);
-  for (int i=0; i<n; i++) {
-    key[i] = rand() % range;
-    printf("%d ",key[i]);
-  }
-  printf("\n");
+    int n = 50;
+    int range = 5;
+    int *key, *bucket;
 
-  std::vector<int> bucket(range); 
-  for (int i=0; i<range; i++) {
-    bucket[i] = 0;
-  }
-  for (int i=0; i<n; i++) {
-    bucket[key[i]]++;
-  }
-  for (int i=0, j=0; i<range; i++) {
-    for (; bucket[i]>0; bucket[i]--) {
-      key[j++] = i;
+    cudaMallocManaged(&key, n * sizeof(int));
+    cudaMallocManaged(&bucket, range * sizeof(int));
+
+    for (int i=0; i<n; i++) {
+        key[i] = rand() % range;
+        printf("%d ", key[i]);
     }
-  }
+    printf("\n");
 
-  for (int i=0; i<n; i++) {
-    printf("%d ",key[i]);
-  }
-  printf("\n");
+    int blockSize = 32;
+    init_bucket<<<(range + blockSize - 1) / blockSize, blockSize>>>(bucket, range);
+    cudaDeviceSynchronize();
+
+    add_bucket<<<(n + blockSize - 1) / blockSize, blockSize>>>(key, bucket, n);
+    cudaDeviceSynchronize();
+
+    for (int i=0, j=0; i<range; i++) {
+        for (; bucket[i]>0; bucket[i]--) {
+            key[j++] = i;
+        }
+    }
+
+    for (int i=0; i<n; i++) {
+        printf("%d ", key[i]);
+    }
+    printf("\n");
+
+    cudaFree(key);
+    cudaFree(bucket);
+
+    return 0;
 }
